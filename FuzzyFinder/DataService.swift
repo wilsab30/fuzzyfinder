4df4d7415cb2127d4fb4c7d8817894fb9166d854
Sabrina Wilbert <sabrinawilbert@gmail.com>
2017-01-16 17:37:33 -0800

    added tableview and detailview for pet profile

--- FuzzyFinder/DataService.swift
+++ FuzzyFinder/DataService.swift
@@ -9,15 +9,25 @@
 import Foundation
 import Firebase
 
+
 let DB_BASE = FIRDatabase.database().reference()
+
+let STORAGE_BASE = FIRStorage.storage().reference()
+
+
 class DataService {
 
     static let ds = DataService()
+    
+    //DB references
     private var _REF_BASE = DB_BASE
     private var _REF_USERS = DB_BASE.child("users")
     private var _REF_PET_PROFILES = DB_BASE.child("pet_profiles")
     private var _REF_OWNER_PROFILE = DB_BASE.child("owner_profile")
     private var _REF_MESSAGES = DB_BASE.child("messages")
+    
+    //Storage refences
+    private var _REF_PET_PROFILE_IMAGES = STORAGE_BASE.child("images")
     
     var REF_BASE: FIRDatabaseReference {
     
@@ -44,6 +54,11 @@ class DataService {
     var REF_MESSAGES: FIRDatabaseReference {
         
         return _REF_MESSAGES
+    }
+    
+    var REF_PET_PROFILE_IMAGES: FIRStorageReference {
+    
+        return _REF_PET_PROFILE_IMAGES
     }
     
     func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
---
+++ FuzzyFinder/PetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PetTableViewCell.swift
+//  FuzzyFinder
+//
+//  Created by Sabrina Wilbert on 1/16/17.
+//  Copyright © 2017 Sabrina Wilbert. All rights reserved.
+//
+
+import UIKit
+
+class PetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
